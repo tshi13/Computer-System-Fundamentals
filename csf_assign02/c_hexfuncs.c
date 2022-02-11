5c356ebf6970d46f5af6cebe8b57d5f800fa64d2
ianzhg <ianzyx@hotmail.com>
2022-02-11 17:07:22 -0500

    Added read and write, not sure if it is right

--- csf_assign02/c_hexfuncs.c
+++ csf_assign02/c_hexfuncs.c
@@ -3,4 +3,18 @@
 #include <unistd.h>  // this is the only system header file you may include!
 #include "hexfuncs.h"
 
-// TODO: add function implementations here
+unsigned hex_read(char data_buf[]) {
+    unsigned count = 0;
+    ssize_t read_result = read(0, data_buf, 16);
+
+    //Return number of bytes read if there is input
+    if(read_result > 0) {
+        count = read_result;
+        return count;
+    }
+    return 0; //Return 0 if there is not input or there is error
+}
+
+void hex_write_string(const char s[]) {
+    write (1, s, 16);
+}
