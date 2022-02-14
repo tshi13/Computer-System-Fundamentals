cc53d2d6a151c27351c369edb3d3824822a12516
ianzhg <ianzyx@hotmail.com>
2022-02-13 20:30:06 -0500

    hex read and write finished

--- csf_assign02/c_hexfuncs.c
+++ csf_assign02/c_hexfuncs.c
@@ -4,17 +4,23 @@
 #include "hexfuncs.h"
 
 unsigned hex_read(char data_buf[]) {
-    unsigned count = 0;
     ssize_t read_result = read(0, data_buf, 16);
 
     //Return number of bytes read if there is input
-    if(read_result > 0) {
-        count = read_result;
-        return count;
-    }
+    if(read_result > 0) return read_result;
     return 0; //Return 0 if there is not input or there is error
 }
 
 void hex_write_string(const char s[]) {
-    write (1, s, 16);
+    //Count how many chars there are in s
+    int count = 0;
+    char curr = s[0];
+    while(curr != '\n') {
+        count++;
+        curr = s[count];
+    }
+    write (1, s, count);
 }
+
+
+
