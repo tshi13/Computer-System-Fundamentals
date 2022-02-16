27e69489c06ff70e9aefbe7ef0bcd0c4586fea18
Ian Zheng <ianzyx@hotmail.com>
2022-02-16 17:55:28 -0500

    fixed some bugs

--- csf_assign02/c_hexmain.c
+++ csf_assign02/c_hexmain.c
@@ -7,25 +7,25 @@ int main(int argc, char*argv[]) {
   //Read user input up to 16 bytes and store it in a string
   char userinput[] = "";
   unsigned num_elements = hex_read(userinput);
-  hex_write_string(userinput);//TODO: this is a trst, DELTE IT
+  hex_write_string(userinput);//TODO: this is a test, DELTE IT
 
   //The output string
-  char output = "";
+  char output[] = "";
 
   //Write the position to output
   hex_format_offset(num_elements, output);
   hex_write_string(output);
   hex_write_string(": ");
   //Write the hex number of each char to output
-  for(int i; i < num_elements; i++) {
+  for(int i = 0; i < num_elements; i++) {
     char cur[] = "";
     hex_format_byte_as_hex(userinput[i], cur);
     hex_write_string(cur);
   }
   hex_write_string("  ");
   //Write the string to output
-  for(int i; i < num_elements; i++) {
-    hex_write_string(hex_to_printable(userinput[i]));
+  for(int i = 0; i < num_elements; i++) {
+    hex_write_string((char*) hex_to_printable(output[i]));
   }
   hex_write_string("\n");
 }
