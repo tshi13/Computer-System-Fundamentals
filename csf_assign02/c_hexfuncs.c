e02445e72dfb803d170ea4fb2a9829661913260d
Ian Zheng <ianzyx@hotmail.com>
2022-02-17 20:47:22 -0500

    "Trying to pull"

--- csf_assign02/c_hexfuncs.c
+++ csf_assign02/c_hexfuncs.c
@@ -5,20 +5,11 @@
 
 unsigned hex_read(char data_buf[]) {
     ssize_t read_result = read(0, data_buf, 16);
-    //Return number of bytes read if there is input
-    if(read_result > 0) return read_result;
-    return 0; //Return 0 if there is not input or there is error
+    return read_result;
 }
 
 void hex_write_string(const char s[]) {
-    //Count how many chars there are in s
-    int count = 0;
-    char curr = s[0];
-    while(curr != '\n') {
-        count++;
-        curr = s[count];
-    }
-    write (1, s, count);
+    write (1, s, 16);
 }
 
 void hex_format_offset(unsigned offset, char sbuf[]) {
