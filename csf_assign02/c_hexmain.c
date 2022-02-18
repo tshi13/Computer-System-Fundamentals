fdb79e2e329e4eb439185418511d468b24f18808
Ian Zheng <ianzyx@hotmail.com>
2022-02-18 12:41:48 -0500

    Can't read less than 16 digits. Need control flow integration

--- csf_assign02/c_hexmain.c
+++ csf_assign02/c_hexmain.c
@@ -6,10 +6,11 @@ int main(int argc, char*argv[]) {
   
   //Read user input up to 16 bytes and store it in a string
   char userinput[16];
-  unsigned num_elements = hex_read(userinput);
+  unsigned num_elements = 16;
   unsigned count = 0;
 
   while(num_elements == 16) {
+      num_elements = hex_read(userinput);
       //The output string
       char output[9];
 
@@ -18,29 +19,35 @@ int main(int argc, char*argv[]) {
       hex_write_string(output);
       hex_write_string(": \0");
 
+      char sentence[16];
       //Write the hex number of each char to output
       for(int i = 0; i < num_elements; i++) {
           char cur[3];
           hex_format_byte_as_hex(userinput[i], cur);
+          sentence[i] = userinput[i];
           hex_write_string(cur);
           hex_write_string(" \0");
       }
-
+      sentence[num_elements] = '\0';
       for(int i = num_elements; i < 16; i++) {
           hex_write_string("   \0");
       }
 
       hex_write_string("  \0");
 
-      //Write the string to output
-      hex_write_string(userinput);
-      for(int i = num_elements; i < 16; i++) {
+      hex_write_string(sentence);
+      if (num_elements != 16) {
+          hex_write_string(".\0");
+      }
+
+      for(int i = num_elements + 1; i < 16; i++) {
           hex_write_string(" \0");
       }
 
       hex_write_string("\n\0");
-      num_elements = hex_read(userinput);
       count += 16;
   }
+
+
 
 }
