003014a81a5c443b7bac3ffebce0f25cf82e70e0
tshi13 <tshi13@jh.edu>
2022-03-08 17:29:57 -0500

    main

---
+++ csf_assign03/main.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main(int argc, char *argv[]){
+  string set_count = argv[1];
+  cout << set_count << endl;
+}
