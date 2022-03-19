87fb8a3631b88ee95d66dd06afbfab67cd1f1dd8
tshi13 <tshi13@jh.edu>
2022-03-19 15:29:11 -0400

    finished styling

--- csf_assign03/main.cpp
+++ csf_assign03/main.cpp
@@ -1,3 +1,10 @@
+/*
+ * main file for cache simulator program
+ * CSF Assignment 3
+ * Taiming Shi, Ian Zheng
+ * tshi13@jh.edu yzheng67@jh.edu
+ */
+
 #include <string>
 #include <iostream>
 #include <map>
@@ -51,40 +58,39 @@ int check_block_count(int parameter) {
 * Given these param, get the save and load command, breakdown the address into tag and index based on set count and block size
 */
 void parse_line(unsigned* command, char* line, int set_count, int block_size) {
-    command[0] = 1; //set 0/1 for command[0] based on save or load
-    if(line[0] == 's') command[0] = 0;
-    bool is_fully_associative = false; //if cache is fully_associative
-    int offset_bits = log2(block_size); //num of bits for offset and index
-    int index_bits = log2(set_count);
-    if (index_bits == 0){ // sets cache to fully_associative if index_bits == 0
-      is_fully_associative = true;
+  command[0] = 1; //set 0/1 for command[0] based on save or load
+  if(line[0] == 's') command[0] = 0;
+  bool is_fully_associative = false; //if cache is fully_associative
+  int offset_bits = log2(block_size); //num of bits for offset and index
+  int index_bits = log2(set_count);
+  if (index_bits == 0){ // sets cache to fully_associative if index_bits == 0
+    is_fully_associative = true;
+  }
+
+  char address[9]; //contains 8 hex characters
+  int i = 4;
+  for (; i < 12; i++) { //extract address part in the input trace file
+    if (line[i] != ' '){
+    address[i - 4] = line[i];
+    } else {
+      break;
     }
+  }
+  address[i-4] = '\0';
 
-    char address[9]; //contains 8 hex characters
-    int i = 4;
-    for (; i < 12; i++) { //extract address part in the input trace file
-        if (line[i] != ' '){
-        address[i - 4] = line[i];
-        } else {
-          break;
-        }
-    }
-    address[i-4] = '\0';
+  unsigned decimal = stoul(address, 0, 16); //convert hexstring to decimal
+  decimal = decimal >> offset_bits; //removes offset bits from address
 
-    unsigned decimal = stoul(address, 0, 16); //convert hexstring to decimal
-    decimal = decimal >> offset_bits; //removes offset bits from address
-
-    int index = 0;
-    if (!is_fully_associative){ //if not fully associative, we do bit shifting to extract index part 
-      int helper_number = pow(2,index_bits) - 1;
-      index = decimal & helper_number;
-      decimal = decimal >> index_bits;
-    }
-    
-    int tag = decimal; // remaining part of decimal is now the tag part
-    command[1] = tag; //put tag and index in correct positions in command
-    command[2] = index;
-
+  int index = 0;
+  if (!is_fully_associative){ //if not fully associative, we do bit shifting to extract index part 
+    int helper_number = pow(2,index_bits) - 1;
+    index = decimal & helper_number;
+    decimal = decimal >> index_bits;
+  }
+  
+  int tag = decimal; // remaining part of decimal is now the tag part
+  command[1] = tag; //put tag and index in correct positions in command
+  command[2] = index;
 }
 
 int main(int argc, char *argv[]){
@@ -133,7 +139,6 @@ int main(int argc, char *argv[]){
     cache.push_back(Set(block_count));
   }
 
-
   char line[15];
   while (scanf(" %[^\n]", line) == 1){ //reading from input file
     unsigned command[3];
@@ -149,58 +154,56 @@ int main(int argc, char *argv[]){
       if(cache[index].find(tag)) hit = true; // see if we can find a slot in set with same tag
       //STORE HIT
       if(hit) {
-          store_hits++;
-          cache[index].mark_slot_as_used(tag); //update respective slot in set as used
-          if(write_through == "write-through") { //write directly to memory
-              total_cycles += 101;
-          } else { //write-back
-              total_cycles++;
-              cache[index].mark_slot_dirty(tag); //if it's write-back we need to set dirty
-          }
+        store_hits++;
+        cache[index].mark_slot_as_used(tag); //update respective slot in set as used
+        if(write_through == "write-through") { //write directly to memory
+          total_cycles += 101;
+        } else { //write-back
+          total_cycles++;
+          cache[index].mark_slot_dirty(tag); //if it's write-back we need to set dirty
+        }
       } else { //STORE MISS
-          store_misses++;
-          //Write allocate
-          if(write_allocate == "write-allocate") { //when we are using write-allocate
-              total_cycles += ((block_size / 4) * 100); //Load happens anyway
-              if(cache[index].set_size == cache[index].block_num) { //set is full, start eviction
-                  if(cache[index].lru_evict()){ //evict slot, if slot is dirty, will need extra total cycles
-                    total_cycles += (block_size / 4) * 100;
-                  }
-              }
-              cache[index].store(tag, false); //store information
-              if(write_through == "write-through") { //write-through, update total cycles
-              total_cycles += 100;
-              total_cycles++;
-              } else { // write-back, update total cycles
-              total_cycles++;
-              cache[index].mark_slot_dirty(tag);
-              }
-          } else { // no write-allocate, update total cycles
-            total_cycles += 100;
+        store_misses++;
+        //Write allocate
+        if(write_allocate == "write-allocate") { //when we are using write-allocate
+          total_cycles += ((block_size / 4) * 100); //Load happens anyway
+          if(cache[index].set_size == cache[index].block_num) { //set is full, start eviction
+            if(cache[index].lru_evict()){ //evict slot, if slot is dirty, will need extra total cycles
+              total_cycles += (block_size / 4) * 100;
+            }
           }
-
-          
-      }
+          cache[index].store(tag, false); //store information
+          if(write_through == "write-through") { //write-through, update total cycles
+          total_cycles += 100;
+          total_cycles++;
+          } else { // write-back, update total cycles
+            total_cycles++;
+            cache[index].mark_slot_dirty(tag);
+          }
+        } else { // no write-allocate, update total cycles
+          total_cycles += 100;
+          }
+        }
     } else if(load_save == 1) { //LOADING
         loads++;
         bool hit = false;
         if(cache[index].find(tag)) hit = true;
         if(hit) { //LOAD HIT, update total cycles
-            load_hits++;
-            total_cycles++;
-            cache[index].mark_slot_as_used(tag); //accessed slot, update all usage sequences in set
+          load_hits++;
+          total_cycles++;
+          cache[index].mark_slot_as_used(tag); //accessed slot, update all usage sequences in set
         } else {
-            load_misses++;
-            total_cycles += (block_size / 4) * 100;
-            total_cycles += 1;
-            if(cache[index].set_size == cache[index].block_num) { //when set is full, need to evict
-                if(cache[index].lru_evict()){ // if the block we evicted was dirty, we need to update total_cycle
-                  total_cycles += (block_size / 4) * 100;
-                }
+          load_misses++;
+          total_cycles += (block_size / 4) * 100;
+          total_cycles += 1;
+          if(cache[index].set_size == cache[index].block_num) { //when set is full, need to evict
+            if(cache[index].lru_evict()){ // if the block we evicted was dirty, we need to update total_cycle
+              total_cycles += (block_size / 4) * 100;
             }
-            cache[index].store(tag, false); // store information in slot
+          }
+          cache[index].store(tag, false); // store information in slot
         }
-    }
+      }
   }
   //output cache simulation results
   cout << "Total loads:" << loads << endl;
--- csf_assign03/set.cpp
+++ csf_assign03/set.cpp
@@ -1,3 +1,10 @@
+/*
+ * cpp file for set class, cache simulator program
+ * CSF Assignment 3
+ * Taiming Shi, Ian Zheng
+ * tshi13@jh.edu yzheng67@jh.edu
+ */
+
 #include "set.h"
 
 #include <map>
@@ -6,49 +13,49 @@ using std::map;
 using namespace std;
 
 bool Set::find(unsigned tag) { //searches if a tag is present in the set
-    std::map<unsigned, slot>::iterator it;
-    it = set.find(tag);
-    if (it == set.end()) return false;
-    return true;
+  std::map<unsigned, slot>::iterator it;
+  it = set.find(tag);
+  if (it == set.end()) return false;
+  return true;
 }
 
 void Set::mark_slot_as_used(unsigned tag) { ////update all usage sequences for slots in set when accessing a slot
-    std::map<unsigned, slot>::iterator curr_slot;
-    std::map<unsigned, slot>::iterator it;
-    curr_slot = set.find(tag);
-    unsigned curr_usage_sequence = (curr_slot->second).usage_sequence;
-    //If the sequence is the hit slot, update it to 0. For older slots, they remain unchanged.
-    //For younger slots, they increment 1. At the end, we still have from 0 to the oldest sequence
-    for(it = set.begin(); it != set.end(); it++) {
-        if((it->second).usage_sequence < curr_usage_sequence) (it->second).usage_sequence++;
-        else if((it->second).usage_sequence == curr_usage_sequence) (it->second).usage_sequence = 0;
-    }
+  std::map<unsigned, slot>::iterator curr_slot;
+  std::map<unsigned, slot>::iterator it;
+  curr_slot = set.find(tag);
+  unsigned curr_usage_sequence = (curr_slot->second).usage_sequence;
+  //If the sequence is the hit slot, update it to 0. For older slots, they remain unchanged.
+  //For younger slots, they increment 1. At the end, we still have from 0 to the oldest sequence
+  for(it = set.begin(); it != set.end(); it++) {
+    if((it->second).usage_sequence < curr_usage_sequence) (it->second).usage_sequence++;
+    else if((it->second).usage_sequence == curr_usage_sequence) (it->second).usage_sequence = 0;
+  }
 }
 
 bool Set::lru_evict() { //evict a block from set when it is full, using LRU
-    std::map<unsigned, slot>::iterator it;
-    bool evict_hit = false; //whether the slot we removed is dirty
-    for(it = set.begin(); it != set.end(); it++) { //iterate through all the slots of the set
-        if(it->second.usage_sequence == block_num - 1) { // find the slot with highest usage sequence (least used)
-            if(it->second.dirty) evict_hit = true ; // if it's dirty, we want to store that back to main memory
-            set.erase(it->first); //remove slot
-            block_num--; //decrease block number count
-            break;
-        }
+  std::map<unsigned, slot>::iterator it;
+  bool evict_hit = false; //whether the slot we removed is dirty
+  for(it = set.begin(); it != set.end(); it++) { //iterate through all the slots of the set
+    if(it->second.usage_sequence == block_num - 1) { // find the slot with highest usage sequence (least used)
+      if(it->second.dirty) evict_hit = true ; // if it's dirty, we want to store that back to main memory
+      set.erase(it->first); //remove slot
+      block_num--; //decrease block number count
+      break;
     }
-    return evict_hit;
+  }
+  return evict_hit;
 }
 
 void Set::store(unsigned tag, bool dirty) { //store new slot with tag into set
-    add_all_sequence(); //add one to usage sequences for all the other slots in set
-    slot new_slot;
-    if(dirty) new_slot.is_dirty(); //checks if new slot should be set to dirty
-    set.insert(pair<unsigned, slot>(tag, new_slot)); //insert new tag-slot pair into set
-    block_num++;
+  add_all_sequence(); //add one to usage sequences for all the other slots in set
+  slot new_slot;
+  if(dirty) new_slot.is_dirty(); //checks if new slot should be set to dirty
+  set.insert(pair<unsigned, slot>(tag, new_slot)); //insert new tag-slot pair into set
+  block_num++;
 }
 
 void Set::mark_slot_dirty(unsigned int tag) { //set the slot containing tag to dirty
-    std::map<unsigned, slot>::iterator it;
-    it = set.find(tag); //find slot, set to dirty
-    it->second.dirty = true;
+  std::map<unsigned, slot>::iterator it;
+  it = set.find(tag); //find slot, set to dirty
+  it->second.dirty = true;
 }
