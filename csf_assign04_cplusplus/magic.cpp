e90d205e9b59f24abe64af484e6150ef3228db1b
ianzhg <ianzyx@hotmail.com>
2022-04-10 18:07:47 -0400

    test

--- csf_assign04_cplusplus/magic.cpp
+++ csf_assign04_cplusplus/magic.cpp
@@ -4,7 +4,7 @@
 #include <cstdarg>
 #include <cerrno>
 #include <cstdint>
-#include <elf.h>
+//#include <elf.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -25,16 +25,19 @@ int main(int argc, char **argv) {
   char* elf_filename = argv[1];
   size_t file_size;
   int fd = open(elf_filename, O_RDONLY);
-  if(fd == -1) cerr << "File cannot be opened" << endl;
+  if(fd == -1) cerr << "File cannot be opened" << endl; return -1;
 
   //Reading bytes from the file
   struct stat statbuf;
   int rc = fstat(fd, &statbuf);
   if (rc != 0) {
     cerr << "Error while reading bytes" << endl;
+    return -1;
   } else {
     file_size = statbuf.st_size;
   }
   void *data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+  cout << "done" << endl;
+
 
 }
